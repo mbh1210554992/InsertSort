00230afa8cf68aedea5275ab5216c094db6a9cad
mbh1210554992 <mbh1210554992@gmail.com>
2019-04-06 22:40:07 +0800

    冒泡算法：增加一个标志位，加强代码效率

--- src/com/ntu/sort/BubbleSortTest.java
+++ src/com/ntu/sort/BubbleSortTest.java
@@ -15,12 +15,20 @@ public class BubbleSortTest {
 	
 	public static void bubbleSort(int[] list){
 		for(int i=0;i<list.length;i++){
+			//添加一个标记（增强性能），如果此次循环没有发生交换，说明排序已经完成，则直接跳出循环
+			boolean flag=true;
 			for(int j=0;j<list.length-i-1;j++){ //此处要注意数组越绝问题
 				if(list[j]>list[j+1]){
 					int temp=list[j];
 					list[j]=list[j+1];
 					list[j+1]=temp;
+					//顺序发生变化时，将标志位改成false
+					flag=false;
 				}
+			}
+			
+			if(flag){
+				break;
 			}
 		}
 	}
